61926d1b667a553794b7efb07b04b723bcea9456
gtrupiano <gtrupiano@oakland.edu>
2024-04-18 20:23:26 -0400

    Commented program

--- Robot_Base_Code/Arduino/I2C_Basic_Send_Test_1/I2C_Basic_Send_Test_1.ino
+++ Robot_Base_Code/Arduino/I2C_Basic_Send_Test_1/I2C_Basic_Send_Test_1.ino
@@ -1,9 +1,12 @@
+// ESP32 Model: DOIT ESP32 DEVKIT V1
+
 #include <Wire.h>
 #include <PS4Controller.h>
 
-int yPos, xPos; // Variables to store joystick positions
-bool rBump, lBump;
-int deadband = 5;
+// Variables to stre controller values
+int xPos, yPos; // Left joystick X and Y position values used for differential drive movement
+bool rBump, lBump; // Left and right bumper states used for E-Stop and Autonomous State respectivly
+int deadband = 5; // Used to offset joystick so resting state is a value of 0
 
 void setup() 
 {
@@ -16,31 +19,29 @@ void setup()
 
 void loop() 
 {
-  // Read joystick positions
-  if (abs(PS4.LStickY()) > deadband)
+  // Read controller values
+  if (abs(PS4.LStickY()) > deadband) // Implements deadband
   {
-    yPos = PS4.LStickY();
+    yPos = PS4.LStickY(); // Reads Y position
   }
   else {
     yPos = 0;
   }
-  if (abs(PS4.LStickX()) > deadband)
+  if (abs(PS4.LStickX()) > deadband) // Implements deadband
   {
-    xPos = PS4.LStickX();
+    xPos = PS4.LStickX(); // Reads X position
   }
   else {
     xPos = 0;
   }
-  rBump = PS4.R1();
-  lBump = PS4.L1();
+  rBump = PS4.R1(); // Reads right bumper state
+  lBump = PS4.L1(); // Reads left bumper state
 
-  // Print joystick positions for debugging
-  /*
+  // Print controller values for debugging
   Serial.print("Y Position: ");
   Serial.println(yPos);
   Serial.print("X Position: ");
   Serial.println(xPos);
-  */
   Serial.print("R1 Button State: ");
   Serial.println(rBump);
   Serial.print("L1 Button State: ");
@@ -55,6 +56,7 @@ void loop()
 void requestEvent() 
 {
   // Convert the integer joystick positions and boolean button state to byte arrays
+  // uint16_t needed as a result of ESP32 and Arduino UNO having different sized integers
   byte yArray[sizeof(uint16_t)];
   byte xArray[sizeof(uint16_t)];
   byte rBumpArray[sizeof(bool)];
