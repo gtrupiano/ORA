8299338abec6b723ec9c1421b34f869aa32ff891
George T. <118488667+gtrupiano@users.noreply.github.com>
2024-05-03 21:44:00 -0400

    Update ZED_Test.py

--- ZED/ZED_Test.py
+++ ZED/ZED_Test.py
@@ -1,8 +1,5 @@
 import pyzed.sl as sl
-import math
-import numpy as np
-import sys
-import math
+
 
 def main():
     # Create a Camera object
@@ -10,51 +7,29 @@ def main():
 
     # Create a InitParameters object and set configuration parameters
     init_params = sl.InitParameters()
-    init_params.depth_mode = sl.DEPTH_MODE.ULTRA  # Use ULTRA depth mode
-    init_params.coordinate_units = sl.UNIT.MILLIMETER  # Use meter units (for depth measurements)
+    init_params.camera_resolution = sl.RESOLUTION.AUTO # Use HD720 opr HD1200 video mode, depending on camera type.
+    init_params.camera_fps = 30  # Set fps at 30
 
     # Open the camera
-    status = zed.open(init_params)
-    if status != sl.ERROR_CODE.SUCCESS: #Ensure the camera has opened succesfully
-        print("Camera Open : "+repr(status)+". Exit program.")
+    err = zed.open(init_params)
+    if err != sl.ERROR_CODE.SUCCESS:
+        print("Camera Open : "+repr(err)+". Exit program.")
         exit()
 
-    # Create and set RuntimeParameters after opening the camera
-    runtime_parameters = sl.RuntimeParameters()
-    
+
+    # Capture 50 frames and stop
     i = 0
     image = sl.Mat()
-    depth = sl.Mat()
-    point_cloud = sl.Mat()
-
-    mirror_ref = sl.Transform()
-    mirror_ref.set_translation(sl.Translation(2.75,4.0,0))
-
+    runtime_parameters = sl.RuntimeParameters()
     while i < 50:
-        # A new image is available if grab() returns SUCCESS
+        # Grab an image, a RuntimeParameters object must be given to grab()
         if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
-            # Retrieve left image
+            # A new image is available if grab() returns SUCCESS
             zed.retrieve_image(image, sl.VIEW.LEFT)
-            # Retrieve depth map. Depth is aligned on the left image
-            zed.retrieve_measure(depth, sl.MEASURE.DEPTH)
-            # Retrieve colored point cloud. Point cloud is aligned on the left image.
-            zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA)
-
-            # Get and print distance value in mm at the center of the image
-            # We measure the distance camera - object using Euclidean distance
-            x = round(image.get_width() / 2)
-            y = round(image.get_height() / 2)
-            err, point_cloud_value = point_cloud.get_value(x, y)
-
-            if math.isfinite(point_cloud_value[2]):
-                distance = math.sqrt(point_cloud_value[0] * point_cloud_value[0] +
-                                    point_cloud_value[1] * point_cloud_value[1] +
-                                    point_cloud_value[2] * point_cloud_value[2])
-                print(f"Distance to Camera at {{{x};{y}}}: {distance}")
-            else : 
-                print(f"The distance can not be computed at {{{x};{y}}}")
-            i += 1    
-           
+            timestamp = zed.get_timestamp(sl.TIME_REFERENCE.CURRENT)  # Get the timestamp at the time the image was captured
+            print("Image resolution: {0} x {1} || Image timestamp: {2}\n".format(image.get_width(), image.get_height(),
+                  timestamp.get_milliseconds()))
+            i = i + 1
 
     # Close the camera
     zed.close()
