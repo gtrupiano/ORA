0219ce1fe1166ce82e49bf2c5b03055486384087
gtrupiano <george.trupiano2@gmail.com>
2024-05-07 02:27:43 -0400

    Created new program that incorperates both motors
    Need to fix math for differential drive and variable names.
    
    Also test if both motors can be controlled using the logic that is set up.
    (setup function)

--- Robot_Base_Code/Arduino/Robot_Base_Arduino_One_Motor/Robot_Base_Arduino_One_Motor.ino
+++ Robot_Base_Code/Arduino/Robot_Base_Arduino_One_Motor/Robot_Base_Arduino_One_Motor.ino
@@ -5,7 +5,7 @@
 
 #define CAN_BAUDRATE 500000
 #define CAN0_INT 2    // Set INT to pin 2 (This is the Interrupt pin)
-#define ODRV0_NODE_ID 0 // Left Motor
+#define ODRV0_NODE_ID 1 // Left Motor
 #define ODRV1_NODE_ID 1 // Right Motor
 
 MCP2515Class& can_intf = CAN;
@@ -17,14 +17,6 @@ MCP2515Class& can_intf = CAN;
 #define IMU_SDA 2 // On AtMega32U4 Connected to Pin 19 (PD1)
 #define IMU_SCL 3 // On AtMega32U4 Connected to Pin 18 (PD0) // CHECK
 
-/*
-// LED Declarations
-#define LEDPWR_R 4 // On AtMega32U4 Connected to Pin 25 (PD4)
-#define LEDPWR_G 12 // On AtMega32U4 Connected to Pin 26 (PD6)
-#define LEDPWR_B 6 // On AtMega32U4 Connected to Pin 27 (PD7)
-*/
-
-// FIX NAME TO ButtonSignal or something like that
 // Robot State Declarations
 #define EStopButtony 4 // On AtMega32U4 Connected to Pin 25 (PD4)
 #define AutonButtony 5 // On AtMega32U4 Connected to Pin 26 (PD6)
@@ -66,8 +58,7 @@ bool setupCan()
 
 // Instantiate ODrive objects
 ODriveCAN odrv0(wrap_can_intf(can_intf), ODRV0_NODE_ID); // Standard CAN message ID
-ODriveCAN odrv1(wrap_can_intf(can_intf), ODRV1_NODE_ID); // Standard CAN message ID
-ODriveCAN* odrives[] = {&odrv0, &odrv1}; // Make sure all ODriveCAN instances are accounted for here
+ODriveCAN* odrives[] = {&odrv0}; // Make sure all ODriveCAN instances are accounted for here
 
 struct ODriveUserData 
 {
@@ -79,7 +70,6 @@ struct ODriveUserData
 
 // Keep some application-specific user data for every ODrive.
 ODriveUserData odrv0_user_data;
-ODriveUserData odrv1_user_data;
 
 // Called every time a Heartbeat message arrives from the ODrive
 void onHeartbeat(Heartbeat_msg_t& msg, void* user_data) 
@@ -112,6 +102,7 @@ void setup()
 
   Wire.begin(8);
 
+
   pinMode(EStop, INPUT);
 
   pinMode(CAN0_INT, INPUT);
@@ -122,11 +113,6 @@ void setup()
   pinMode(EStopButtony, OUTPUT);
   pinMode(AutonButtony, OUTPUT);
 
-  /*
-  pinMode(LEDPWR_R, OUTPUT);
-  pinMode(LEDPWR_G, OUTPUT);
-  pinMode(LEDPWR_B, OUTPUT);
-  */
   pinMode(Battery_Voltage, INPUT);
 
 
@@ -142,9 +128,6 @@ void setup()
   odrv0.onFeedback(onFeedback, &odrv0_user_data);
   odrv0.onStatus(onHeartbeat, &odrv0_user_data);
 
-  odrv1.onFeedback(onFeedback, &odrv1_user_data);
-  odrv1.onStatus(onHeartbeat, &odrv1_user_data);
-
 
   if (!setupCan()) 
   {
@@ -153,7 +136,7 @@ void setup()
   }
 
   Serial.println("Waiting for ODrive...");
-  while (!odrv0_user_data.received_heartbeat || !odrv1_user_data.received_heartbeat) 
+  while (!odrv0_user_data.received_heartbeat) 
   {
     pumpEvents(can_intf);
     delay(100);
@@ -169,27 +152,13 @@ void setup()
     Serial.println("vbus request failed!");
     while (true); // spin indefinitely
   }
-  else if (!odrv1.request(vbus, 1))
-  {
-    Serial.println("vbus request failed!");
-    while (true); // spin indefinitely
-  }
-
-  Serial.print("DC voltage [V]: ");
-  Serial.println(vbus.Bus_Voltage);
-  Serial.print("DC current [A]: ");
-  Serial.println(vbus.Bus_Current);
 
   Serial.println("Enabling closed loop control...");
-  while (odrv0_user_data.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL || odrv1_user_data.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL) 
+  while (odrv0_user_data.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL) 
   {
     odrv0.clearErrors();
     delay(1);
     odrv0.setState(ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL);
-
-    odrv1.clearErrors();
-    delay(1);
-    odrv1.setState(ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL);
 
     // Pump events for 150ms. This delay is needed for two reasons;
     // 1. If there is an error condition, such as missing DC power, the ODrive might
@@ -204,6 +173,7 @@ void setup()
       pumpEvents(can_intf);
     }
   }
+  Serial.println("test");
 
   Serial.println("ODrive running!");
 }
@@ -211,7 +181,6 @@ void setup()
 void loop() 
 {
   pumpEvents(can_intf); // This is required on some platforms to handle incoming feedback CAN messages
-
   Wire.requestFrom(8, sizeof(int) + sizeof(int) + sizeof(bool) + sizeof(bool)); // request data from slave device #8 (ESP32)
   
   // Checks to see if data is recieved over I2C. If so sets values from controller to predefined variables
@@ -242,7 +211,6 @@ void loop()
     Serial.print("Received from ESP32 - EStop Button: ");
     Serial.println(EStopButton);
     */
-
     delay(10);
   } 
   else
@@ -251,6 +219,7 @@ void loop()
     horizontalMov = 0;
     EStopState = false;
     AutonState = false;
+
     Serial.println("Insufficient bytes received");
   }
 
@@ -272,6 +241,7 @@ void loop()
     digitalWrite(EStopButtony, LOW);
   }
 
+
   if (AutonButton)
   {
     AutonState = true;
@@ -283,8 +253,8 @@ void loop()
     AutonState = false;
     digitalWrite(AutonButtony, LOW);
   }
-
-  /*
+  
+/*
   // print position and velocity for Serial Plotter
   if (odrv0_user_data.received_feedback) 
   {
@@ -293,60 +263,45 @@ void loop()
     Serial.print("ODrive 0 Velocity:");
     Serial.println(feedback.Vel_Estimate);
   }
-
-  if (odrv1_user_data.received_feedback) 
-  {
-    Get_Encoder_Estimates_msg_t feedback = odrv1_user_data.last_feedback;
-    odrv1_user_data.received_feedback = false;
-    Serial.print("ODrive 1 Velocity:");
-    Serial.println(feedback.Vel_Estimate);
-  }
-  */
+*/
+  Serial.print("EStop State: ");
+  Serial.println(EStopState);
+  Serial.print("Auton State: ");
+  Serial.println(AutonState);
+  Serial.println();
+  delay(10);
 }
 
-void ODriveMovement(double verticalVelocity, double horizontalVelocity) // Fix later
+// Converts Joystick values into Revolutions/Second and sends data to ODrive over CAN
+void ODriveMovement(double verticalVelocity, double horizontalVelocity) // redo math for turns/second
 {
   // Scales inputs to directional vector
-  verticalVelocity = constrain(verticalVelocity, -1.0, 1.0);
+  verticalVelocity = constrain(verticalVelocity, -50, 50);
   horizontalVelocity = constrain(horizontalVelocity, -1.0, 1.0);
 
   // Standard for differential drive control
   double leftMotorVel = verticalVelocity - horizontalVelocity;
   double rightMotorVel = verticalVelocity + horizontalVelocity;
 
-  // Gearbox ratio
-  const double gearboxRatio = 21.0;
-
-  // Input RPM
-  const double inputRPM = 4400.0;
-
-  // Converts the velocity requested into Turns / second
-  const double maxVelocity = 5.0; //  Speed limit according to IGVC rules
-  double leftMotorRPS = leftMotorVel * maxVelocity; // Turns/second
-  double rightMotorRPS = rightMotorVel * maxVelocity; // Turns/second
-
-  // Adjust for gearbox ratio and input RPM
-  leftMotorRPS = (leftMotorRPS / gearboxRatio) * (inputRPM / 60.0);
-  rightMotorRPS = (rightMotorRPS / gearboxRatio) * (inputRPM / 60.0);
+  // Converts the velocity requested into Revolutions / second
+  const double maxVelocity = 5.0; // IGVC rules, should be meters/second units but not sure
+  const double wheelCircumference = 2 * M_PI * 0.1524; // Circumference of the wheel in meters
+  double leftMotorRPS= (leftMotorVel * maxVelocity) / wheelCircumference; // Turns/second
+  double rightMotorRPS = (rightMotorVel * maxVelocity) / wheelCircumference; // Turns/second
 
   // Send velocity CAN commands to left and right motors
-  odrv0.setVelocity(leftMotorRPS);
-  odrv1.setVelocity(rightMotorRPS);
+  odrv0.setVelocity(verticalVelocity);
 }
 
 // Send CAN command to ODrive to initate EStop
 void ODriveEStop()
 {
   Serial.println("Enabling EStop...");
-  while (odrv0_user_data.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_IDLE || odrv1_user_data.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_IDLE) 
+  while (odrv0_user_data.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_IDLE) 
   {
     odrv0.clearErrors();
     delay(1);
     odrv0.setState(ODriveAxisState::AXIS_STATE_IDLE);
-
-    odrv1.clearErrors();
-    delay(1);
-    odrv1.setState(ODriveAxisState::AXIS_STATE_IDLE);
 
     // Pump events for 150ms. This delay is needed for two reasons;
     // 1. If there is an error condition, such as missing DC power, the ODrive might
@@ -368,15 +323,11 @@ void ODriveEStop()
 void ODriveControlState() // Could combine with estop function but meh
 {
   Serial.println("Enabling closed loop control...");
-  while (odrv0_user_data.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL || odrv1_user_data.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL) 
+  while (odrv0_user_data.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL) 
   {
     odrv0.clearErrors();
     delay(1);
     odrv0.setState(ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL);
-
-    odrv1.clearErrors();
-    delay(1);
-    odrv1.setState(ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL);
 
     // Pump events for 150ms. This delay is needed for two reasons;
     // 1. If there is an error condition, such as missing DC power, the ODrive might
